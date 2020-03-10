587aee9898bbc410eaae6eb616d14b4bdec63604
Vladimir1203 <vladimir12934@gmail.com>
2020-03-10 18:58:14 +0100

    Izgenerisana JavaDoc HTML strane.

--- VulkanizerskaRadnja2020/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja2020/src/gume/radnja/VulkanizerskaRadnja.java
@@ -4,9 +4,27 @@ import java.util.LinkedList;
 
 import gume.AutoGuma;
 
+/**
+ * Klasa koja opisuje Vulkanizesku radnju i neke njene funkcionalnosti.
+ * 
+ * @author Vladimir Lazic
+ * @version 0.0.2
+ */
 public class VulkanizerskaRadnja {
+	/**
+	 * Lista guma dostupnih na stanju.
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	/**
+	 * Dodaje gumu u postojecu listu.
+	 * 
+	 * @param a nova guma.
+	 * @throws java.lang.RuntimeException     baca izuzetak u slucaju da guma vec
+	 *                                        postoji u listi.
+	 * @throws java.lang.NullPointerException baca izuzetak u slucaju da je uneta
+	 *                                        guma null.
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -15,12 +33,18 @@ public class VulkanizerskaRadnja {
 		gume.addFirst(a);
 	}
 
+	/**
+	 * Pronalazi gume odredjenog modela i vraca ih kao novu listu.
+	 * 
+	 * @param markaModel ime marke i modela.
+	 * @return novaLista guma(sve gume koje imaju trazeni naziv markaModel).
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
 		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
 		for (int i = 0; i < gume.size(); i++)
-			if (gume.get(i).equals(markaModel))
+			if (gume.get(i).getMarkaModel().equals(markaModel))
 				novaLista.add(gume.get(i));
 		return novaLista;
 	}
